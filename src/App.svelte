db9a5a2024cac40f9fdf84f2ab2405e29b3c315a
JohnDennehy <johndennehy101@gmail.com>
2021-05-06 23:23:43 +0100

    Added store for navigator. Added router for handling app routes. Added components folder with navigator and title components.

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,26 @@
     import {onMount, getContext} from 'svelte'
     import { UserService } from "./user-service";
     import { setContext } from "svelte";
+    import Navigator from "./components/Navigator.svelte";
+    import Router from "svelte-spa-router";
 
     const url = `http://jd-2.local:4000/api/users`;
 
     setContext("UserService", new UserService("http://jd-2.local:4000"));
+
+
+  import {navBar, mainBar, subTitle, title} from "./stores"
+
+  title.set("Donation Services Inc.");
+  subTitle.set("All Current Candidates");
+  navBar.set({
+    bar: mainBar
+  });
+
+    let routes = {
+    //"/signup": Candidates,
+    //"/login": Donations,
+  }
 
    
 //This would be moved out to externals
@@ -22,3 +38,9 @@
   })
   </script>
 
+<div class="uk-container">
+  <Navigator/>
+
+   <Router {routes} />
+
+</div>
---
+++ src/components/Navigator.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Title from "./Title.svelte";
+  import {navBar} from "../stores"
+</script>
+
+<div class="uk-flex uk-flex-center uk-flex-middle uk-text-center uk-grid-small" uk-grid>
+  <Title title="Point of Interest Application" subTitle="Sign Up or Log In" />
+  <div>
+    <ul class="uk-iconnav uk-margin-left">
+
+    {#each $navBar.bar as nav}
+        <a href="{nav.link}" class="uk-card uk-card-small uk-card-hover uk-padding-small">
+          <i class="{nav.icon}" style="{nav.colour}" uk-tooltip="title: {nav.title}; pos:bottom"></i>
+        </a>
+      {/each}
+
+    <!-- <a href="/#/signup" class="uk-card-hover uk-padding-small">
+        <i class="fas fa-user-cog fa-3x" style="color:rgb(63, 160, 155)" title="Sign up" pos="bottom" uk-tooltip></i>
+    </a>
+    <a href="/#/login" class="uk-card-hover uk-padding-small">
+        <i class="fas fa-user-edit fa-3x" style="color:green" title="Log in" pos="bottom" uk-tooltip></i>
+    </a> -->
+</ul>
+  </div>
+</div>
