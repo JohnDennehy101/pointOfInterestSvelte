1d8527c3336d453942f01c133e6b4e1bc25dd904
JohnDennehy <johndennehy101@gmail.com>
2021-05-13 23:16:45 +0100

    Added components to reports page for filtering. Logic still to be implemented for functionality to be available to users.

---
+++ src/components/SearchTitlesForm.svelte
@@ -0,0 +1,18 @@
+<form
+  style="display:inline-block"
+  class="uk-search uk-search-default uk-width-3-5"
+>
+  <a
+    href="/report"
+    id="searchIcon"
+    class="uk-search-icon-flip"
+    type="submit"
+    uk-search-icon
+  />
+  <input
+    class="uk-search-input"
+    id="searchInputField"
+    type="search"
+    placeholder="Search"
+  />
+</form>
--- src/pages/Report.svelte
+++ src/pages/Report.svelte
@@ -1,6 +1,8 @@
 <script>
   import { title, subTitle, navBar, loggedInUserBar } from "../stores";
   import MonumentList from "../components/MonumentList.svelte";
+  import FilterComponent from "../components/FilterComponent.svelte";
+  import SearchTitlesForm from "../components/SearchTitlesForm.svelte";
   title.set("Monuments");
   subTitle.set("Monuments added to date");
 
@@ -9,6 +11,27 @@
   });
 </script>
 
+<div class="uk-container uk-margin">
+  <div class="uk-grid-small" uk-grid>
+    <div class="uk-width-expand@m">
+      <h1 class="uk-heading">Monuments Added To Date</h1>
+    </div>
+    <div class="uk-width-auto@m">
+      <!-- {{> filterComponent}} -->
+
+      <FilterComponent />
+      <SearchTitlesForm />
+    </div>
+  </div>
+
+  <div class="uk-width-auto@m">
+    <div class="uk-margin">
+      <!-- {{> searchTitlesForm}} -->
+    </div>
+  </div>
+</div>
+
+<div class="uk-divider-icon" />
 <div
   class="uk-child-width-expand uk-flex-center uk-flex-middle uk-text-center"
   uk-grid
