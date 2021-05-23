15f497e7ac261d2a3d6e054a5efea178cb739c8a
JohnDennehy <johndennehy101@gmail.com>
2021-05-23 19:40:53 +0100

    Attempting default icon to resolve bug of non-existent marker image.

--- src/services/leaflet-map.ts
+++ src/services/leaflet-map.ts
@@ -100,8 +100,19 @@ export class LeafletMap {
 
   addMarker(location: Location, popupText = '', layerTitle = 'default') {
     let group: LayerGroup;
+
+    var defaultIcon = L.icon({
+    iconUrl: '/images/marker-icon.png',
+    shadowUrl: '/images/marker-icon.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
     
-    let marker = L.marker([location.lat, location.lng]);
+    let marker = L.marker([location.lat, location.lng], {icon: defaultIcon});
     if (popupText) {
       var popup = L.popup({ autoClose: false, closeOnClick: false });
       popup.setContent(popupText);
